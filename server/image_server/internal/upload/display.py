1d4d6f705f87171bd54aed9c95b358de8af7e48e
minhquanBr2 <minhquanhole03@gmail.com>
2024-07-27 16:43:03 +0000

    add get all endpoint to image sv

--- server/image_server/internal/upload/display.py
+++ server/image_server/internal/upload/display.py
@@ -1,14 +1,2 @@
 import config
 import requests
-
-async def get_all_images():
-    url = f"{config.DB_ENDPOINT_URL}/select/all_images"
-    response = requests.get(url)
-    if response.status_code != 200:
-        return {"message": f"Internal server error with status code {response.status_code}."}
-
-    results = response.json()["message"]
-    if len(results) == 0:
-        return {"message": "No images found."}
-
-    return results
